3801282e9179456fff63dd8cd46fef69f0ea62b6
ymouse91 <35890980+ymouse91@users.noreply.github.com>
2026-05-26 14:35:53 +0300

    Publish site from docs folder

---
+++ docs/sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'restart-v13';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './fonts/fredoka-one-latin.woff2',
+  './fonts/nunito-latin.woff2'
+];
+
+self.addEventListener('install', e =>
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  )
+);
+
+self.addEventListener('activate', e =>
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  )
+);
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
